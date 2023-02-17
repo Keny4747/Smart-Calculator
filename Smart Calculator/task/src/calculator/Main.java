58d7e44629a5e55678b5dd7a8f5755512085fb2c
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-02-17 10:42:08 -0500

    ...

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,112 +1,56 @@
 package calculator;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    private static boolean isUnknownCommand(String command) {
+        return command.startsWith("/") && !command.equals("/exit") && !command.equals("/help");
+    }
 
     public static void main(String[] args) {
-        Map<String,Integer> listVariables = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("");
-            String input = scanner.nextLine();
-
-
-            switch (input) {
-                case "/exit" -> {
-                    System.out.println("Bye!");
-                    System.exit(0);
-                }
-                case "/help" -> {
-                    System.out.println("The program calculates the sum of numbers");
-                    continue;
-                }
-                case "/go"->{
-                    System.out.println("Unknown command");
-                    continue;
-                }
-                case "" -> {
-                    continue;
-                }
+        String input;
+        Expression expression;
+        do {
+            input = scanner.nextLine();
+            if (Expression.isInvalid(input)) {
+                System.out.println("Invalid expression");
+                continue;
             }
-            if(input.contains("=")){
-              String [] result = Test.addVariable(input,listVariables).split(" ");
-                if(result.length>1){
-                    listVariables.put(result[0],Integer.parseInt(result[1]));
-                }
-              continue;
+            if (isUnknownCommand(input)) {
+                System.out.println("Unknown command");
+                continue;
             }
-
-            if(input.contains("-")||input.contains("+")){
-                String changedVl=Test.obtainValue(input,listVariables);
-                try {
-                    int result = eval(changedVl);
-                    System.out.println(result);
-                } catch (Exception e) {
-                    System.out.println("Invalid expression");
+            expression = new Expression(input);
+            if (expression.isVariableAssignment()) {
+                expression.storeVariable();
+                continue;
+            }
+            if (expression.isVariableQuery()) {
+                if (Expression.isValidIdentifier(input)) {
+                    String value = expression.getVariables().getOrDefault(input, "Unknown variable");
+                    System.out.println(value);
+                } else {
+                    System.out.println("Invalid identifier");
                 }
                 continue;
             }
-            if(listVariables.containsKey(input.trim())){
-                System.out.println(listVariables.get(input));
-
-            }else {
-                System.out.println("Unknown variable");
+            switch (input) {
+                case "":
+                    break;
+                case "/exit":
+                    System.out.println("Bye!");
+                    break;
+                case "/help":
+                    System.out.println("The program calculates the sum/subtraction/multiplication/division of numbers");
+                    break;
+                default:
+                    try {
+                        System.out.println(expression.evaluate());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
             }
-
-        }
-    }
-
-
-    public static int eval(String expr) {
-        String regExSum = "[+]+|[-]{2}|-{4}";
-        String regEsSub = "[-]{3}";
-
-        String cambiar = expr.replaceAll(regEsSub, "-")
-                .replaceAll(regExSum, "+")
-                .replaceAll(" ", "")
-                .replaceAll("-{4,}", "+")
-                .replaceAll("\\+{4,}", "+")
-                .replaceAll("-\\+{1}", "-")
-                .replaceAll("\\+-{1}", "+");
-
-        List<String> lista = addContent(cambiar);
-        String[] tokens = lista.toArray(new String[0]);
-        int result = Integer.parseInt(tokens[0]);
-        for (int i = 1; i < tokens.length; i += 2) {
-            String op = tokens[i];
-            int value = Integer.parseInt(tokens[i + 1]);
-            switch (op) {
-                case "+" -> result += value;
-                case "-" -> result -= value;
-                default -> throw new IllegalArgumentException("Invalid expression");
-            }
-        }
-        return result;
-    }
-
-    public static List<String> addContent(String inputUser) {
-
-        List<String> elements = new ArrayList<>();
-        StringBuilder currentNumber = new StringBuilder();
-        int j = 0;
-        if (inputUser.charAt(0) == '+' || inputUser.charAt(0) == '-') {
-            elements.add("0");
-            elements.add(String.valueOf(inputUser.charAt(0)));
-            j = elements.size() - 1;
-        }
-        for (int i = j; i < inputUser.length(); i++) {
-            char c = inputUser.charAt(i);
-            if (c == '+' || c == '-') {
-                elements.add(currentNumber.toString());
-                elements.add(String.valueOf(c));
-                currentNumber = new StringBuilder();
-            } else {
-                currentNumber.append(c);
-            }
-        }
-        elements.add(currentNumber.toString());
-
-        return elements;
+        } while (!input.equals("/exit"));
     }
 }
